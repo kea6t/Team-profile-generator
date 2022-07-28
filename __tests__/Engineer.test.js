81d73f9a99bd03d7bef559936c887d47a78829b4
kea6t <keasay26@gmail.com>
2022-07-28 05:26:08 -0400

    refactored Manager.tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,22 +2,24 @@ const Engineer = require('../lib/Engineer');
 
 
 describe('Engineer', () => {
+    // const variables for testing
+    const name = 'Peter'
+    const id = 102;
+    const email = 'iamafake@email.com';
+    const github = 'iamgithub';
+    const engineer = new Engineer(name, id, email, github);
+
+    // test for creating username
     describe('username', () => {
         it('Should create a github username for engineer', () => {
-            const username = 'iamgithub';
-            const email = 'iamafake@email.com';
-            const engineer = new Engineer('Dave', 102, email, username);
 
-            expect(engineer.github).toBe(username);
+            expect(engineer.github).toBe(github);
         });
     });
 
     // test for getGithub() function
     describe('github', () => {
         it('Should get the github username from getGithub() function', () => {
-            const github = 'iamgithub';
-            const email = 'iamafake@email.com';
-            const engineer = new Engineer('Dave', 102, email, github);
 
             expect(engineer.getGithub()).toBe(github);
         });
@@ -26,9 +28,6 @@ describe('Engineer', () => {
     // test for getRole() function
     describe('role', () => {
         it('Should get the role of a Engineer', () => {
-            const email = 'iamafake@email.com';
-            const github = 'iamgithub';
-            const engineer = new Engineer('Dave', 102, email, github);
 
             expect(engineer.getRole()).toBe('Engineer');
         });
