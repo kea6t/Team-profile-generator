b1d0b87e4c6f7cd1a5cdec16f6e2216a82afeb0b
kea6t <keasay26@gmail.com>
2022-07-28 05:22:12 -0400

    refactored Employee/Intern and added new files for generating site and page template

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,14 @@
 const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
+    // const variables for testing
+    const name = 'Dave'
+    const id = 101;
+    const email = 'iamafake@email.com';
+    const employee = new Employee(name, id, email);
     // test for creating Employee object
     describe('employee', () => {
         it('Creates a employee object', () => {
-            const employee = new Employee('Dave');
 
             expect(employee.name).toEqual(expect.any(String));
             expect(employee.name.length).toBeGreaterThan(0);
@@ -14,8 +18,6 @@ describe('Employee', () => {
     // test for creating Employee id 
     describe('id', () => {
         it('Creates a employee id', () => {
-            const id = '001';
-            const employee = new Employee('Dave', id);
 
             expect(employee.id).toBe(id);
         });
@@ -24,8 +26,6 @@ describe('Employee', () => {
     // test for creating Employee email
     describe('email', () => {
         it('Creates a employee email', () => {
-            const email = 'iamafake@email.com';
-            const employee = new Employee('Dave', 101, email);
 
             expect(employee.email).toBe(email);
         });
@@ -34,7 +34,6 @@ describe('Employee', () => {
     // test for getName() function
     describe('getName', () => {
         it('Gets a employee name from getName() function', () => {
-            const employee = new Employee('Dave');
 
             expect(employee.getName()).toBe('Dave');
         });
@@ -43,8 +42,6 @@ describe('Employee', () => {
     // test for getId() function
     describe('getId', () => {
         it('Gets employee id from getId() function', () => {
-            const id = '001';
-            const employee = new Employee('Dave', id);
 
             expect(employee.getId()).toBe(id);
         });
@@ -53,8 +50,6 @@ describe('Employee', () => {
     // test for getRole() function
     describe('getRole', () => {
         it('Gets the role of a employee', () => {
-            const email = 'iamafake@email.com';
-            const employee = new Employee('Dave', 101, email);
 
             expect(employee.getRole()).toBe('Employee');
         });
--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -2,13 +2,15 @@ const Intern = require('../lib/Intern');
 
 
 describe('Intern', () => {
+    // const variables for testing
+    const name = 'Cat';
+    const id = 103;
+    const school = 'GMU';
+    const email = 'iamafakestudent@email.com';
+    const intern = new Intern(name, id, email, school);
     // test for creating school
     describe('school', () => {
         it('Creates a school for intern', () => {
-            const school = 'GMU';
-            const email = 'iamafakestudent@email.com';
-            const intern = new Intern('Cat', 103, email, school);
-
             expect(intern.school).toBe(school);
         });
     });
@@ -16,9 +18,6 @@ describe('Intern', () => {
     // test for getSchool() function
     describe('getSchool', () => {
         it('Should get the school name from getGithub() function', () => {
-            const school = 'GMU';
-            const email = 'iamafakestudent@email.com';
-            const intern = new Intern('Cat', 103, email, school);
 
             expect(intern.getSchool()).toBe(school);
         });
@@ -27,10 +26,6 @@ describe('Intern', () => {
     // test for getRole() function
     describe('role', () => {
         it('Should get the role of a Intern', () => {
-            const school = 'GMU';
-            const email = 'iamafakestudent@email.com';
-            const intern = new Intern('Cat', 103, email, school);
-
             expect(intern.getRole()).toBe('Intern');
         });
     });
